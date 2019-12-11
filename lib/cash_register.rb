6aa7163cbd3c0161297cf15966f9b0f44776f2eb
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-11 21:32:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -40,7 +40,8 @@ class CashRegister
      if index < @transactions.length do 
     self.total -= @transactions.pop 
     index += 1 
-    elsif @transactions.length == 0 do 
+    end 
+    if @transactions.length = 0
     self.total == 0 
     end
     end 
