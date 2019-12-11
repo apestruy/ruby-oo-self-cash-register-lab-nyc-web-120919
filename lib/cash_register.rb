f7baf20abe4bf4988212be10600dae608eef6a5f
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-11 20:28:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
     @total -= (total * discount / 100)
     return "After the discount, the total comes to $#{@total}."
   else 
-    return ""
+    return "There is no discount to apply."
   end 
   
   
