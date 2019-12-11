d365b3e0efe73ff105c584c735b882a6ad769996
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-11 20:14:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,9 @@
 class CashRegister
   attr_accessor :total, :emp_discount
   
-  def initialize(emp_discount = "")
+  def initialize(emp_discount = 0)
     @total = 0
-    @emp_discount
+    @emp_discount = 
   end 
   
   
